3ed4ea014c5843fe37235717b3f35dcc6f3f9c35
m23ck <andojomack@gmail.com>
2019-10-27 17:25:26 -0300

    added basefile, templates and mysql, still gotta work on functionality, worked on db

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
-from flask import Flask, render_template, url_for
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, url_for, redirect
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
 
-@app.route("/")
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'presentie_app'
+
+mysql = MySQL(app)
+
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
     return render_template("index.html")
 if __name__ == "__main__":
     app.run(debug=True)
+
+        CREATE TABLE 'presentie' (
+    'id' int NOT NULL AUTOINCREMENT,
+    'studentid' int NOT NULL,
+    'vakid' int NOT NULL,
+    'datum' date NOT NULL,
+    'periode' int NOT NULL
+    );
+
+
+# ALTER TABLE 'presentie'
+#   ADD CONSTRAINT FOREIGN KEY (`klant_id`) REFERENCES `klant` (`klant_id`),
+#   ADD CONSTRAINT FOREIGN KEY (`freelancer_id`) REFERENCES `freelancers` (`freelancer_id`),
