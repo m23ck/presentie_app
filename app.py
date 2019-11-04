b1eecd563306c2293b6f5abfb9caf2fbc207497e
m23ck <andojomack@gmail.com>
2019-11-04 11:53:36 -0300

    fixed bugs, mysql works, cnnections et

--- app.py
+++ app.py
@@ -3,20 +3,23 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'presentie_app'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 mysql = MySQL(app)
 
-
 @app.route("/", methods=['POST', 'GET'])
+
 def index():
+    cur = mysql.connection.cursor()
+       
     if request.method == 'POST':
         pass
     else:
         return render_template("index.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
-
